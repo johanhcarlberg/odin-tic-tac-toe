d96e9d3bd3d5dd31fcf199d9ff9ac41253ee38ad
JSHC <johan.hansson.carlberg@gmail.com>
2022-12-17 13:19:19 +0100

    Add basic render function

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ const gameBoardModule = (() => {
     }
 
     function render() {
-
+        for (let index in _gameBoard) {
+            console.log(_gameBoard[index]);
+            const item = _gameBoard[index];
+            const itemDiv = document.createElement('div');
+            itemDiv.classList.add('game-board-item')
+            itemDiv.textContent = item;
+            _gameBoardContainer.appendChild(itemDiv);
+        }
     }
+
+    render();
 
     return {
         getGameBoard,
