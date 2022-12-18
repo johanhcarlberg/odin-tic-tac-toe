d1006bd24ddff66b24bb3f99b309234fd184abb5
JSHC <johan.hansson.carlberg@gmail.com>
2022-12-18 11:53:34 +0100

    Add functionality to check win state

--- script.js
+++ script.js
@@ -32,12 +32,19 @@ const game = (() => {
     })();
 
     const gameBoardModule = (() => {
-        const _gameBoard = [
-            'X', '', 'O',     
-            '', 'X', '',
-            '', '', ''
-        ];
-        const _gameBoardContainer = document.querySelector("#game-board-container");
+        let _gameBoard = [];
+
+        function _reset() {
+            _gameBoard = [
+                '', '', '',
+                '', '', '',
+                '', '', '',
+            ]
+        }
+
+        function init() {
+            _reset();
+        }
 
         function getGameBoard() {
             return _gameBoard;
@@ -51,7 +58,8 @@ const game = (() => {
 
         return {
             getGameBoard,
-            setGameBoardItemAtIndex
+            setGameBoardItemAtIndex,
+            init
         }
     })();
 
@@ -95,18 +103,79 @@ const game = (() => {
     const _player1 = playerFactory(1, 'X');
     const _player2 = playerFactory(2, 'O');
     let _currentPlayer = _player1;
+    let _isGameOver = false;
 
     function takeTurn(gameBoardIndex) {
+        if (_isGameOver) {
+            return;
+        }
         console.log(_currentPlayer);
         if (gameBoardModule.getGameBoard()[gameBoardIndex] !== '') {
             console.log(`index ${gameBoardIndex} already occupied`);
             return;
         }
         gameBoardModule.setGameBoardItemAtIndex(gameBoardIndex, _currentPlayer.symbol);
+        if (checkWinState()) {
+            console.log(`Player ${_currentPlayer.id} wins!`)
+            _isGameOver = true;
+        }
         _currentPlayer === _player1 ? _currentPlayer = _player2 : _currentPlayer = _player1;
     }
+
+    function checkWinState() {
+        const _gameBoard = gameBoardModule.getGameBoard();
+        if (checkRows(_gameBoard) || checkCols(_gameBoard)) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    function checkCols(_gameBoard) {
+        for (let i = 0; i < 3; i++) {
+            let colMatches = 1;
+            let colSymbol = _gameBoard[i];
+            if (!colSymbol) {
+                continue;
+            }
+            for (let j = 1; j < 3; j++) {
+                let colCurrIndex = i + j * 3;
+                if (_gameBoard[colCurrIndex] === colSymbol) {
+                    colMatches++;
+                }
+            }
+            if (colMatches === 3) {
+                console.log(`player with symbol ${colSymbol} wins (cols)`);   
+                return true;
+            }
+        }
+        return false;
+    }
     
+    function checkRows(_gameBoard) {
+        for (let i = 0; i < 3; i++) {
+            let rowMatches = 1;
+            let rowSymbol = _gameBoard[i * 3];
+            if (!rowSymbol) {
+                continue;
+            }
+            for (let j = 1; j < 3; j++) {
+                let rowCurrIndex = i * 3 + j;
+                if (_gameBoard[rowCurrIndex] === rowSymbol) {
+                    rowMatches++;
+                }
+            }
+            if (rowMatches === 3) {
+                console.log(`player with symbol ${rowSymbol} wins (rows)`);   
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    gameBoardModule.init();
     displayModule.init(gameBoardModule.getGameBoard());
     
+    return { checkWinState };
 })();
 
